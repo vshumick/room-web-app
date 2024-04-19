080cfedc2262d836f0ccd4f9c26f64b86b7e0d1b
vshumick <vshumick@gmail.com>
2024-04-19 15:25:44 +0300

    room-web-app

---
+++ src/main/java/com/example/roomwebapp/service/RoomService.java
@@ -0,0 +1,23 @@
+package com.example.roomwebapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.roomwebapp.models.Room;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    private static final List<Room> rooms = new ArrayList();
+
+    static{
+        for(int i=0;i<10;i++){
+            rooms.add(new Room(i, "Room " + i, "R"+i, "Q"));
+        }
+    }
+
+    public List<Room> getAllRooms(){
+        return rooms;
+    }
+}
